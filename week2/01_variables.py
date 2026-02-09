effac49a22150802323fcc31c368a7200cd4de3e
Ali <asimae@udel.edu>
2026-02-09 12:03:44 -0500

    Define Variables

---
+++ week2/01_variables.py
@@ -0,0 +1,12 @@
+APP_TITLE = "Database Assignment Manager"
+
+assignment_title = "Homework1: Intro to Database Design"
+assignment_description= "Create an ER Model"
+due_date = "2026-1-26"
+is_published = False
+
+print("Title: ", assignment_title)
+print("Description: ",assignment_description)
+
+print("------ f-string use:")
+print(f"Title: {assignment_title}\nDescription: {assignment_description}")
