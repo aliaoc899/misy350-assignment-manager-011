b50693c08e8f84b5735edea6feb14fc3b02bd497
Ali <asimae@udel.edu>
2026-02-09 12:21:31 -0500

    intro to dictionaries

---
+++ week2/03_dict.py
@@ -0,0 +1,7 @@
+assignment = {
+        "title":"Homework1: Intro to Database Design",
+        "description":"Create an ER Model",
+        "due_date": "2026-01-26",
+        "is_published": False,
+        "questions":[]
+} #dictionary
