8ea9be946628645fc219b4d3a86f599f67dc25e6
Ali <asimae@udel.edu>
2026-02-09 12:13:09 -0500

    Lists

---
+++ week2/02_lists.py
@@ -0,0 +1,12 @@
+titles = ["Homework1: Intro to Database Design",
+          "Homework2: Normalization",
+          "Case Study: Entity Requirements"]
+
+print("Titles: ",titles)
+
+titles.append("Project Milestone")
+
+print("---")
+print("Titles: ",titles)
+
+print("Second Assignment Title:", titles[1])
