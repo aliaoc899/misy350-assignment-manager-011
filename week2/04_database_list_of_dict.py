80741004be1025c767dd7e9ec13f39ab7f533aaa
Ali <asimae@udel.edu>
2026-02-11 12:06:47 -0500

    list of dictionaries - Database

---
+++ week2/04_database_list_of_dict.py
@@ -0,0 +1,30 @@
+course_data = [
+    {
+        "id": "HW1",
+        "title": "Homework 1: intro to database",
+        "description": "learning the basics",
+        "due_date" : "2026-01-26",
+        "score": 100,
+    },
+    {
+        "id": "HW2",
+        "title": "Homework2: Normalization",
+        "description": "learning to normalize",
+        "due_date" : "2026-01-28",
+        "score" : 100
+    }
+]
+
+course_data.append(
+    {
+        "id" : "HW3",
+        "title" : "Case Study",
+        "description" : "comprehansive analysis of db",
+        "due_date" : "2026-02-10",
+        "score" : 100   
+    }
+)
+
+print(f"assignments : {course_data}")
+
+
